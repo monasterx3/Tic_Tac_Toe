b9ecf1d622250bbbd996b27fdb933852bec406fe
monasterx3 <38019200+monasterx3@users.noreply.github.com>
2019-06-26 19:27:50 -0400

    Add files via upload
    How to play page

---
+++ Activity2.java
@@ -0,0 +1,30 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGame();
+            }
+        });
+    }
+public void openGame(){
+    Intent intent = new Intent(this, MainActivity.class);
+    startActivity(intent);
+}
+}
